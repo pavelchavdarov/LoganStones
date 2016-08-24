e80298af8be0f7e8c3cf80ebb80ad95cf0781fd5
pavelchavdarov <pavelchavdarov@yandex.ru>
2016-08-24 17:53:55 +0600

    pouch finished

--- src/LoganStonesGame/Pouch.py
+++ src/LoganStonesGame/Pouch.py
@@ -5,6 +5,9 @@ Created on 5 июля 2016 г.
 '''
 from LoganStonesGame.Entity import GemEntity
 from LoganStonesGame.Gem import Gem
+from _socket import SHUT_RD
+from random import shuffle
+from random import choice
 
 
 
@@ -20,9 +23,21 @@ class Pouch(object):
         '''
         Constructor
         '''
-        self.stones = []
+        self.gems = []
         self.stone_count = Pouch.STONE_COUNT
         GemEntity.init_entities(Pouch.ENTITY_COUNT)
         for i in range(Pouch.STONE_COUNT/Pouch.ENTITY_COUNT):
             for ent in GemEntity.get_entities():
-                self.stones.append(Gem(ent, GemEntity.get_entity_relation(ent)[0]))
+                self.gems.append(Gem(ent, GemEntity.get_entity_relation(ent)[0]))
+    
+    def p_shuffle(self):
+        shuffle(self.gems)
+    
+    def f_pull_out_gem(self):
+        l_gem = choice(self.gems)
+        self.gems.remove(l_gem)
+        return l_gem
+    
+    def f_gems_inside(self):
+        return self.gems.__len__() 
+        
