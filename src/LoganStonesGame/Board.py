a21fcb90cef4caf77cf442f2231ff1708f99bf96
Pavel Chavdarov <pavelchavdarov@yandex.ru>
2016-08-27 01:33:47 +0600

    refactoring

--- src/LoganStonesGame/Board.py
+++ src/LoganStonesGame/Board.py
@@ -1,30 +1,27 @@
-class Cell_Position (object):
-	# для кубических координат
+
+ 
+class Board (object):		
 	#! специфично для куб-коодинат
 	directions = tuple([(0,1,-1),(1,0,-1),(1,-1,0),(0,-1,1),(-1,0,1),(-1,1,0)])
-
-	# применяется только для фишек на поле (доске)
-	#! специфично для куб-коодинат
-	def check_position(self, position):
-		if position[0]+position[1]+position[2]!=0:
+	
+	@staticmethod
+	def get_neighbours(p_position): # (x, y, z)
+		neighbours = set()
+		for i in range(len(Board.directions)):
+			# надо бобавить проверку на лостижение границы поля, width & high 
+			_x = p_position[0] + Board.directions[i][0]
+			_y = p_position[1] + Board.directions[i][1]
+			_z = p_position[2] + Board.directions[i][2]
+			neighbours.add((_x,_y,_z))
+		return neighbours
+	
+	@staticmethod
+	def check_position(p_position):
+		if p_position[0]+p_position[1]+p_position[2]!=0:
 			result = 1 # ошибка: сумма координат должна быть равна 0
 		else:
 			result = 0 # позиция задана корректно
 		return result
-	
-	#возвращает множество координат соседей
-	def get_neighbours(self):
-		neighbours = set()
-		for i in range(len(Cell_Position.directions)):
-			_x = self.X + Cell_Position.directions[i][0]
-			_y = self.Y + Cell_Position.directions[i][1]
-			_z = self.Z + Cell_Position.directions[i][2]
-			neighbours.add((_x,_y,_z))
-		return set(neighbours)
-
-#############################################
-#все правила постановки фишек, проверку достижения цели (4 в ряд) будет делать "игровая доска" 
-class Board (Cell_Position):		
 	
 	def __init__(self):
 		self.gems = dict() # камни на доске
